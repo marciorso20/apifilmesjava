da304ff983ce00539715c1ddf0f9229be6acc67e
marciorso20 <marciorso20@gmail.com>
2023-03-28 15:05:58 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,38 @@
+import java.net.http.HttpClient;
+import java.net.URI;
+
+import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    private static JsonParser jsonParser;
+
+    public static void main(String[] args) throws Exception {
+
+        // Conexão HTTP e buscar o TOP de 250 filmes na API
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        var response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // Extrair os dados (Titulo, poster e classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        // System.out.println(listaDeFilmes.size());
+        // System.out.println(listaDeFilmes.get(0));
+
+        // Exibir e manipular dados
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
+    }
+}
